a6dae3b9d714d5dac702010479569ee37be7a203
pjc0179 <pjc0179@gmail.com>
2024-12-18 05:04:47 +0900

    feat:"헤더 Link 추가"

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
+import Link from "next/link";
 import React from "react";
 
 const Header = () => {
   return (
     <header className=" h-[60px] flex items-center">
-      <div>
+      <Link href={"/"}>
         <Image src={"/image/logo.png"} alt="로고" height={40} width={151} />
-      </div>
+      </Link>
     </header>
   );
 };
